fda72294a0efe1513e8a988488da6fcd6180f66b
GYR <1434898122@qq.com>
2022-09-06 09:42:51 +0800

    开始正式学习

---
+++ day2/src/Main.java
@@ -0,0 +1,19 @@
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("1100/5");
+        Scanner sc = new Scanner(System.in);
+//       int a=sc.nextInt();
+//       int b=sc.nextInt();
+//       int c=a+b;
+//        System.out.println(c);
+        int sum=0,i=0,j=0;
+        for (i=0;i<=100;i++) {
+            sum=sum+i;
+}
+        System.out.println(sum);
+    }
+}
